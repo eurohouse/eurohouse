abd86dc2fd2b696602147707bae29451db87097f
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-06-15 12:25:44 +1000

    Latest update

--- mode.bookkeeping.php
+++ mode.bookkeeping.php
@@ -1,4 +1,4 @@
 <!-- book -->
-<!-- RU: Поваренная книга; CN: 烹饪手册; KR: 요리 매뉴얼; JP: 調理マニュアル; AE: كتاب الطبخ -->
+<!-- RU: Бухгалтерский учёт; CN: 簿记和会计; KR: 장부 및 회계; JP: 簿記と会計; AE: إمساك الدفاتر -->
 <div class='customPanel' id='bookkeep_disp' style="width:100%;height:85%;left:0px;top:0px;overflow-y:scroll;">
 </div>
