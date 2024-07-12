0121cff83f85632ccb4de9d8180daea18fa61a95
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-07-12 12:54:09 +1000

    Latest update

--- mode.font_book.php
+++ mode.font_book.php
@@ -1,5 +1,5 @@
 <!-- font -->
-<!-- GR: Προβολή γραμματοσειρών; CY: Προβολή γραμματοσειρών; DE: Schriftartenbetrachter; FR: Visionneuse de polices; IT: Visualizzatore di caratteri; IN: फ़ॉन्ट दर्शक; LK: फॉन्ट दर्शक; RU: Средство просмотра шрифта; NP: ཡིག་གཟུགས་ལྟ་མཁན།; RS: Прегледач фонтова; ES: Visor de fuentes; PT: Visualizador de fontes; MX: Visor de fuentes; BR: Visualizador de fontes; TR: Yazı tipi görüntüleyici; UA: Засіб перегляду шрифту; CN: 字体查看器; KR: 글꼴 뷰어; JP: フォントビューア; AE: كتاب الخطوط -->
+<!-- GR: Προβολή γραμματοσειρών; CY: Προβολή γραμματοσειρών; DE: Schriftartenbetrachter; FR: Visionneuse de polices; IT: Visualizzatore di caratteri; IN: फ़ॉन्ट दर्शक; LK: फॉन्ट दर्शक; RU: Средство просмотра шрифта; NP: ཡིག་གཟུགས་ལྟ་མཁན།; RS: Прегледач фонтова; ES: Visor de fuentes; PT: Visualizador de fontes; RO: Cartea cu fonturi; MD: Cartea cu fonturi; MX: Visor de fuentes; BR: Visualizador de fontes; TR: Yazı tipi görüntüleyici; UA: Засіб перегляду шрифту; CN: 字体查看器; KR: 글꼴 뷰어; JP: フォントビューア; AE: كتاب الخطوط -->
 <!-- <ref> -->
 <!-- true -->
 <h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['specimen'];?></h1>
