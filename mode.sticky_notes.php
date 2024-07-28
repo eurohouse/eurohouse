d50940f794acedf139201cc1a495bdbe6f9afcf9
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-07-28 18:25:24 +1000

    Added new menu

---
+++ mode.sticky_notes.php
@@ -0,0 +1,4 @@
+<!-- note -->
+<!-- GR: Σημειώσεις; CY: Σημειώσεις; FR: Remarques; BE: Remarques; DE: Anmerkungen; AT: Anmerkungen; CH: Explorare Notae; IT: Note adesive; ES: Notas adhesivas; MX: Notas adhesivas; PT: Lembretes; BR: Lembretes; RO: Note lipicioase; MD: Note lipicioase; RU: Заметки; NP: སྦྱར་བའི་དྲན་ཐོ།; RS: Напомене; UA: Замітки; IN: स्टिकी नोट; TR: Yapışkan notlar; LK: चिपचिपा टिप्पणियाँ; CN: 便利贴; KR: 부착 노트; JP: ポストイット; AE: ورق ملاحظات -->
+
+
