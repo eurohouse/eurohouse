35a240069a85adca50b97c0cac37a468482340ac
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-08-08 10:23:03 +1000

    Added new menu

---
+++ mode.calculator.php
@@ -0,0 +1,4 @@
+<!-- calc -->
+<!-- GR: Αριθμομηχανή; CY: Αριθμομηχανή; FR: Calculatrice; BE: Calculatrice; DE: Taschenrechner; AT: Taschenrechner; CH: Computus; IT: Calcolatore; ES: Calculadora; MX: Calculadora; PT: Calculadora; BR: Calculadora; RO: Calculatorul; MD: Calculatorul; RU: Калькулятор; NP: རྩིས་འཕྲུལ་ཆས།; RS: Калкулатор; UA: Калькулятор; IN: कैलकुलेटर; TR: Hesap makinesi; LK: गणकम्; CN: 计算器; KR: 계산자; JP: 電卓; AE: آلة حاسبة -->
+
+
