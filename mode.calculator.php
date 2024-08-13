f7f558bd542505a781a3bf6eb929ce580a7d3b99
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-08-13 16:57:07 +1000

    Updated Calculator

--- mode.calculator.php
+++ mode.calculator.php
@@ -5,8 +5,8 @@
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='|';" value="|">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='~';" value="~">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='^';" value="^">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='x';" value="x">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='A';" value="A">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='<';" value="<">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='<=';" value="<=">
 </p>
 <p align='center'>
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='%';" value="%">
@@ -14,35 +14,35 @@
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='(';" value="(">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+=')';" value=")">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='=';" value="=">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='B';" value="B">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='==';" value="==">
 </p>
 <p align='center'>
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='7';" value="7">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='8';" value="8">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='9';" value="9">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='/';" value="/">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='y';" value="y">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='C';" value="C">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='>';" value=">">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='>=';" value=">=">
 <br>
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='4';" value="4">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='5';" value="5">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='6';" value="6">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='*';" value="*">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='!=';" value="!=">
 <input type="image" class="power" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;top:14px;" onclick="document.getElementById('omniBox').value = ''; document.getElementById('omniBox').focus();" src="<?=$prefix.'backspace.png'.$suffix;?>">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='D';" value="D">
 <br>
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='1';" value="1">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='2';" value="2">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='3';" value="3">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='-';" value="-">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='x';" value="x">
 <input type="image" class="power" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;top:14px;" onclick="document.getElementById('omniBox').focus();" src="<?=$prefix.'keyboard.png'.$suffix;?>">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='E';" value="E">
 <br>
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='0';" value="0">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='.';" value=".">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+=',';" value=",">
 <input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='+';" value="+">
+<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='y';" value="y">
 <input type="image" class="power" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;top:14px;" onclick="omniEnter();" src="<?=$prefix.'return.png'.$suffix;?>">
-<input type="button" onmouseover="soundButton();" style="width:40px;height:40px;position:relative;" onclick="omniBox.value+='F';" value="F">
 </p>
 
