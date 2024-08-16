a61f18b76610dded2971ef1bb90e201816957c56
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-08-16 16:04:41 +1000

    Updated macroses and font display tool

--- mode.font_book.php
+++ mode.font_book.php
@@ -2,14 +2,9 @@
 <!-- GR: Προβολή γραμματοσειρών; CY: Προβολή γραμματοσειρών; DE: Schriftartenbetrachter; AT: Schriftartenbetrachter; CH: Typographia Liber; FR: Visionneuse de polices; BE: Visionneuse de polices; IT: Visualizzatore di caratteri; IN: फ़ॉन्ट दर्शक; LK: फॉन्ट दर्शक; RU: Средство просмотра шрифта; NP: ཡིག་གཟུགས་ལྟ་མཁན།; RS: Прегледач фонтова; ES: Visor de fuentes; PT: Visualizador de fontes; RO: Cartea cu fonturi; MD: Cartea cu fonturi; MX: Visor de fuentes; BR: Visualizador de fontes; TR: Yazı tipi görüntüleyici; UA: Засіб перегляду шрифту; CN: 字体查看器; KR: 글꼴 뷰어; JP: フォントビューア; AE: كتاب الخطوط -->
 <!-- <ref> -->
 <!-- true -->
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['specimen'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_ascii'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_latin'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_phone'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_greek'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_cyril'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_arabi'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_korea'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_china'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_other'];?></h1>
-<h1 align='center' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_emoji'];?></h1>
+<p style="font-size:24pt;" align='left' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_'.(($settings['pangram'][$session['units']]) ? $settings['pangram'][$session['units']] : $settings['pangram']['default'])];?></p>
+<p style="font-size:22pt;" align='left' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_'.(($settings['pangram'][$session['units']]) ? $settings['pangram'][$session['units']] : $settings['pangram']['default'])];?></p>
+<p style="font-size:20pt;" align='left' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_'.(($settings['pangram'][$session['units']]) ? $settings['pangram'][$session['units']] : $settings['pangram']['default'])];?></p>
+<p style="font-size:18pt;" align='left' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_'.(($settings['pangram'][$session['units']]) ? $settings['pangram'][$session['units']] : $settings['pangram']['default'])];?></p>
+<p style="font-size:16pt;" align='left' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_'.(($settings['pangram'][$session['units']]) ? $settings['pangram'][$session['units']] : $settings['pangram']['default'])];?></p>
+<p style="font-size:14pt;" align='left' class="<?=(in_array($request['input'], ['','.','..','./','./.','./..'])) ? 'euro' : 'userDefine';?>"><?=$session['pangram_'.(($settings['pangram'][$session['units']]) ? $settings['pangram'][$session['units']] : $settings['pangram']['default'])];?></p>
