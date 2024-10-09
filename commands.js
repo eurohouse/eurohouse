63abb6775a49a8131b14cd078e000754adb9846a
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-09 11:16:14 +1000

    Latest update

--- commands.js
+++ commands.js
@@ -23,20 +23,13 @@ function executeMacros(input, index = 0, length = 1) {
                 if (nameInc >= namePart2) { omniListen(museLint[i], true); break; } nameInc++;
             } omniPause();
         }
-    } else if ((index == (length - 1)) && (input.includes("\\/")) && (input.startsWith('\\/'))) {
-        var nameNum = input.replace("\\/", ''), museMelo = hex2bin(sysDefMelody.value);
-        var museLint = (sysDefMusicBox.value).split('//');
-        var museInd = arraySearch(((museMelo.startsWith(requestPath.value+'/')) ? museMelo.replace(requestPath.value+'/','') : museMelo), museLint); console.log(museInd);
-        var museNum = museLint.length, museDiv = Math.floor(museNum / parseInt(nameNum));
-        var museNxt = (museInd+museDiv)%museNum;
-        console.log(museNxt);
-        omniListen(museLint[museNxt], true);
     } else if ((index == (length - 1)) && (input.includes('./')) && (input.startsWith('./'))) {
         omniRead(requestMode.value, input.replace('./', ''), requestLock.value);
     } else if ((index == (length - 1)) && (input.includes('*'))) {
         omniDisp(requestMode.value, input.replace('*', ''), requestLock.value);
     } else if ((index == (length - 1)) && (input.includes('@'))) {
-        var atr = input.split('@'), atd, atx; if (atr[0].includes(':')) {
+        var atr = input.split('@'), atd, atx;
+        if (atr[0].includes(':')) {
             atd = atr[0].split(':'); atx = CryptoJS.SHA256(atd[1]).toString();
             if (atr[1].includes('signin')) {
                 omniAuthRequest('signin', atd[0], atx);
