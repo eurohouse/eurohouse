f609522cb3fa8c46803014aa99fce1ab4f6d5ccb
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-10 16:46:07 +1000

    Latest update

--- mode.local_user_manager.php
+++ mode.local_user_manager.php
@@ -1,10 +1,9 @@
 <!-- access -->
 <!-- GR: Τοπικοί χρήστες και ομάδες; DE: Lokale Benutzer und Gruppen; AT: Lokale Benutzer und Gruppen; CY: Τοπικοί χρήστες και ομάδες; CH: Administrare Usuarem; ES: Usuarios locales y grupos; MX: Usuarios locales y grupos; FR: Utilisateurs et groupes locaux; BE: Utilisateurs et groupes locaux; TR: Yerel kullanıcılar ve gruplar; IT: Utenti e gruppi locali; RO: Utilizatori și grupuri locale; MD: Utilizatori și grupuri locale; LK: स्थानीयप्रयोक्तारः समूहाः च; IN: स्थानीय उपयोगकर्ता और समूह; RU: Локальные пользователи и группы; RS: Локални корисници и групе; NP: ས་གནས་ཀྱི་སྤྱོད་མཁན་དང་སྡེ་ཚན།; BR: Usuários e grupos locais; PT: Utilizadores e grupos locais; UA: Локальні користувачі та групи; CN: 本地用户和组; KR: 로컬 사용자; JP: ローカルユーザ; AE: المستخدمون المحليون -->
-<?php $line1Size = 60;
-$relBind = str_replace('_session.json', '', $allUsers);
+<?php $relBind = str_replace('_session.json', '', $allUsers);
 foreach ($relBind as $key=>$value) {
     $relTitle = (@json_decode(file_get_contents($value.'_session.json'), true) != null) ? json_decode(file_get_contents($value.'_session.json'), true)['title'] : $value; ?><p align='center' class='block'>
-    <input type="button" value="<?=$value.'@'.$websiteID.':'.$request['path'].(($value == 'root')?'#':'$');?>" style="width:<?=$line1Size;?>%;" onmouseover="soundButton();"><?php if (isAuth()) { ?>
+    <input type="button" value="<?=$value.'@'.$websiteID.':'.$request['path'].(($value == 'root')?'#':'$');?>" style="width:70%;" onmouseover="soundButton();"><?php if (isAuth()) { ?>
         <input type="image" name="<?=$value;?>" onmouseover="soundButton();" class="power" onclick="this.src = (isFriends(this.name))?sysDefPrefix.value+'user.png':sysDefPrefix.value+'anonym.png'; toggleFriend(this.name);" src="<?=$prefix.'user.png';?>">
     <?php if ($sessionID == 'root') { ?>
         <input type="image" name="<?=$value;?>" onmouseover="soundButton();" class="power" onclick="delete_user(this.name); window.location.reload();" src="<?=$prefix.'delete.png';?>">
