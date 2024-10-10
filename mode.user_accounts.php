ca98f26aa4b24cd702f2940b1e374c23339c75fc
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-10 15:06:30 +1000

    Latest update

--- mode.user_accounts.php
+++ mode.user_accounts.php
@@ -1,15 +1,17 @@
 <!-- user -->
 <!-- GR: Λογαριασμοί χρηστών; CY: Λογαριασμοί χρηστών; DE: Benutzerkonten; AT: Benutzerkonten; FR: Comptes utilisateur; BE: Comptes utilisateur; CH: Explorare Usuarem; IT: Profili utente; LK: उपयोक्तृलेखाः; TR: Kullanıcı hesapları; IN: उपयोगकर्ता खाते; PT: Contas de utilizador; BR: Contas de usuário; ES: Cuentas de usuario; MX: Cuentas de usuario; RO: Conturi de utilizator; MD: Conturi de utilizator; UA: Облікові записи користувачів; NP: སྤྱོད་མཁན་གྱི་ཁ་བྱང་།; RU: Учётные записи пользователей; RS: Кориснички налози; CN: 用户账户经理; KR: 사용자 계정; JP: ユーザーアカウント; AE: حسابات المستخدمين -->
 <?php $preStyle = "white-space:pre-wrap;word-wrap:break-word;";
-$iconSize = 50; $relBind = str_replace('_session.json', '', $allUsers); ?>
+$relBind = str_replace('_session.json', '', $allUsers); ?>
 <table style="width:100%;" id="table"><thead><tr>
-    <th style="width:16%;<?=$preStyle;?>"><a href="javascript:SortTable(0, 'T');"><?=term('Name', $settings['vocabulary'], $session['units']);?></a></th><th style="width:12%;<?=$preStyle;?>">
-    <a href="javascript:SortTable(1, 'T');"><?=term('Username', $settings['vocabulary'], $session['units']);?></a></th>
-    <th style="width:5%;"><?=term('Actions', $settings['vocabulary'], $session['units']);?></th></tr></thead>
-    <tbody><?php foreach ($relBind as $key=>$value) {
-        $cookieLeft = (@json_decode(file_get_contents($value.'_session.json'), true) != null) ? json_decode(file_get_contents($value.'_session.json'), true) : $defaultUserData; ?>
-        <tr><td><?=$cookieLeft['title'];?></td>
-        <td><?=$value;?></td><td><p align='center' class='block'>
+    <th style="width:15%;<?=$preStyle;?>"><a href="javascript:SortTable(0, 'T');"><?=term('Name', $settings['vocabulary'], $session['units']);?>
+    </a></th>
+    <th style="width:10%;<?=$preStyle;?>"><a href="javascript:SortTable(1, 'T');"><?=term('Username', $settings['vocabulary'], $session['units']);?></a></th>
+    <th style="width:5%;"><?=term('Actions', $settings['vocabulary'], $session['units']);?></th>
+    </tr></thead><tbody><?php foreach ($relBind as $key=>$value) {
+        $relTitle = (@json_decode(file_get_contents($value.'_session.json'), true) != null) ? json_decode(file_get_contents($value.'_session.json'), true)['title'] : $value; ?>
+        <tr><td><?=$relTitle;?></td>
+        <td><?=$value;?></td><td>
+        <p align='center' class='block'>
         <input type="image" name="<?=$value;?>" onmouseover="soundButton();" class="power" onclick="bind(sysDefSessionID.value, this.name);" src="<?=$prefix.'chain.png';?>">
         <input type="image" name="<?=$value;?>" onmouseover="soundButton();" class="power" onclick="this.src = (isFriends(this.name))?sysDefPrefix.value+'user.png':sysDefPrefix.value+'anonym.png'; toggleFriend(this.name);" src="<?=$prefix.'user.png';?>">
         <?php if (isAuth()) {
@@ -23,6 +25,6 @@ $iconSize = 50; $relBind = str_replace('_session.json', '', $allUsers); ?>
             <?php }
         }} else { ?>
         <input type="image" name="<?=$value;?>" onmouseover="soundButton();" class="power" onclick="omniPath(this.name+'_session.json', '', 'false');" src="<?=$prefix.'info.png';?>">
-    <?php } ?></p></td>
-</tr><?php } ?>
+    <?php } ?></p>
+</td></tr><?php } ?>
 </tbody></table>
