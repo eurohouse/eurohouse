3d50883b28e82d2ac29e47f20313bd669402b968
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-23 14:35:23 +1000

    Update

--- active_hours.php
+++ active_hours.php
@@ -1,8 +1,13 @@
 <?php
 include 'functions.php';
 $userSettings = fileopen('settings.json');
-$usersList = str_replace('./.store/','',(glob('./.store/*_store.json')));
-$allContent = ""; foreach ($usersList as $key=>$value) {
-    $jsonTestArr = arropen($value.'_session.json', json_encode($userSettings['defaults']), 'DEFAULT');
-    $allContent .= $value."\r\n".json_encode($jsonTestArr)."\r\n\r\n";
-} echo $allContent; // Read All Lines At Once
+$usersList = str_replace('./.store/','',(glob('./*_session.json')));
+$allContent = ""; $othCT = $othAH = '';
+foreach ($usersList as $key=>$value) {
+    $testArr = arropen($value, json_encode($userSettings['defaults']), 'DEFAULT');
+    $othTZ = dec_tz($testArr['timezone']);
+    date_default_timezone_set($othTZ);
+    $othCT .= date('H')." ";
+    $othAH .= implode(' ', explode(',', $testArr['active_hours']))."\r\n";
+} echo $othCT."\r\n\r\n". // Read Line 0
+$othAH; // Read Line 1
