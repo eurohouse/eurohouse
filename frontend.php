1f049830a21a5e5ed40b05796821e76941447776
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-23 18:51:23 +1000

    Kj

--- frontend.php
+++ frontend.php
@@ -291,7 +291,7 @@ function storeOpen(id) {
     var userArr = (sysDefUsersList.value).split(',');
     var userNum = arraySearch(id, userArr);
     var hours = storeHours(id).split(',');
-    return typeof(hours);
+    return (hours);
 }
 function userTimeNow(id) {
     var userArr = (sysDefUsersList.value).split(',');
