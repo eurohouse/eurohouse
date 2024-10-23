c5cfd5d07a3276272461274658cf20b22cc233ff
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-23 14:23:17 +1000

    Test

--- active_hours.php
+++ active_hours.php
@@ -3,6 +3,6 @@ include 'functions.php';
 $userSettings = fileopen('settings.json');
 $usersList = str_replace('./.store/','',(glob('./.store/*_store.json')));
 $allContent = ""; foreach ($usersList as $key=>$value) {
-    $jsonTestArr = arropen($value.'_session.json', $userSettings['defaults'], 'DEFAULT');
+    $jsonTestArr = arropen($value.'_session.json', json_encode($userSettings['defaults']), 'DEFAULT');
     $allContent .= $jsonTestArr['active_hours']."\r\n\r\n";
 } echo $allContent; // Read All Lines At Once
