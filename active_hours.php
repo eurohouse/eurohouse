d81d20ba9b6c5edfa33008cbf887f119ff9795c4
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-23 14:08:35 +1000

    Latest update

--- active_hours.php
+++ active_hours.php
@@ -4,7 +4,7 @@ $userSettings = fileopen('settings.json');
 $usersList = str_replace('./.store/','',(glob('./.store/*_store.json'))); $allContent = ""; foreach ($usersList as $key=>$value) {
     $otherData = arropen($value.'_session.json', json_encode($userSettings['defaults']), 'DEFAULT');
     $otherTimezone = dec_tz($otherData['timezone']); date_default_timezone_set($otherTimezone);
-    $hoursAct = $otherData['active_hours']."\r\n";
+    $hoursAct = $otherData['active_hours']." ";
     $hoursNow = date('H')." ";
 } echo $hoursNow."\r\n\r\n". // Read Line 0
 $hoursAct; // Read Line 1
