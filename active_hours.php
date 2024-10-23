278f496b2e6466b57dec5763b3855c0946c814e9
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-23 14:07:31 +1000

    Fixes

--- active_hours.php
+++ active_hours.php
@@ -4,8 +4,7 @@ $userSettings = fileopen('settings.json');
 $usersList = str_replace('./.store/','',(glob('./.store/*_store.json'))); $allContent = ""; foreach ($usersList as $key=>$value) {
     $otherData = arropen($value.'_session.json', json_encode($userSettings['defaults']), 'DEFAULT');
     $otherTimezone = dec_tz($otherData['timezone']); date_default_timezone_set($otherTimezone);
-    $otherHours = explode(',', $otherData['active_hours']);
-    $activeHoursPage = implode(' ', $otherHours)."\r\n";
-    $localHoursNow = date('H')." ";
-} echo $localHoursNow."\r\n\r\n"; // Read Line 0
-$activeHoursPage; // Read Line 1
+    $hoursAct = $otherData['active_hours']."\r\n";
+    $hoursNow = date('H')." ";
+} echo $hoursNow."\r\n\r\n". // Read Line 0
+$hoursAct; // Read Line 1
