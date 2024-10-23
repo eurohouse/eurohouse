1724922d7ffcc9004c4624aed98f35adc3febec2
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-23 14:25:46 +1000

    Latest

--- active_hours.php
+++ active_hours.php
@@ -4,5 +4,5 @@ $userSettings = fileopen('settings.json');
 $usersList = str_replace('./.store/','',(glob('./.store/*_store.json')));
 $allContent = ""; foreach ($usersList as $key=>$value) {
     $jsonTestArr = arropen($value.'_session.json', json_encode($userSettings['defaults']), 'DEFAULT');
-    $allContent .= $jsonTestArr."\r\n\r\n";
+    $allContent .= json_encode($jsonTestArr)."\r\n\r\n";
 } echo $allContent; // Read All Lines At Once
