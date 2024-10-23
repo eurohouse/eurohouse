1cd2a0f9beb64843a4574cfb6c9785d76cf2e276
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-10-23 14:18:34 +1000

    Latest update

--- active_hours.php
+++ active_hours.php
@@ -1,9 +1,7 @@
 <?php
 include 'functions.php';
-$usersList = str_replace('./.store/','',(glob('./.store/*_store.json'))); $allContent = ""; foreach ($usersList as $key=>$value) {
-    $otherData = fileopen($value.'_session.json');
-    $otherTimezone = dec_tz($otherData['timezone']); date_default_timezone_set($otherTimezone);
-    $hoursAct = $otherData['active_hours']." ";
-    $hoursNow = date('H')." ";
-} echo $otherData['timezone']."\r\n\r\n". // Read Line 0
-$otherData['active_hours']; // Read Line 1
+$usersList = str_replace('./.store/','',(glob('./.store/*_store.json')));
+$allContent = ""; foreach ($usersList as $key=>$value) {
+    $jsonTestArr = jsonopen($value.'_session.json', true);
+    $allContent .= $jsonTestArr."\r\n\r\n";
+} echo $allContent; // Read All Lines At Once
