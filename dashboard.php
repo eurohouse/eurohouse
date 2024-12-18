9bb7c2fcb95ed5ab80e0d385b4674208a6019e25
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-12-18 10:15:25 +1000

    Added new functionality

--- dashboard.php
+++ dashboard.php
@@ -76,11 +76,12 @@
     <input type="image" id="buttonPlay" onmouseover="soundButton();" class="power" onclick="if (sysDefPlaying.value == 1) { omniPause();
     } else { omniListen(dtw(sysDefMelody.value, sysDefSessionID.value, 'あいうえおかがきぎぐけげこごさざしじすずせぜそぞただちぢづてでとどなにぬねのはばぱひびぴふぶぷべぺほぼぽまみむめもやゆよらりるれろわゐゑをんゔゟ')); }" src="<?=$prefix.'play.png';?>">
     <input type="image" onmouseover="soundButton();" id="buttonPitched" class="power" onclick="setdata('pitch_lock', flip(sysDefPitchLock.value));" src="<?=$prefix.(($session['pitch_lock'] != 0)?'midi.png':'volume.png');?>">
-    <select id="avatarPicker" style="width:18%;" onchange="setdata('avatar', avatarPicker.options[avatarPicker.selectedIndex].id); if (sysDefReload.value != 0) { window.location.reload(); }">
+    <select id="avatarPicker" style="width:8%;" onchange="setdata('avatar', avatarPicker.options[avatarPicker.selectedIndex].id); if (sysDefReload.value != 0) { window.location.reload(); }">
     <?php foreach ($userLocks['avatar'] as $key=>$value) { ?>
     <option id="<?=explode('.', $value)[1];?>" <?php if ($session['avatar'] == explode('.', $value)[1]) { ?> selected <?php } ?>>
         <?=explode('.', $value)[1];?>
     </option><?php } ?></select>
+    <input type="image" id="buttonMuteBack" onmouseover="soundButton();" class="power" onclick="setdata('loop', flip(sysDefLoop.value));" src="<?=$prefix.'disk.png';?>">
     <input type="image" id="buttonAlarm" onmouseover="soundButton();" class="power" onclick="if (alarmPlayer.paused) {
         playAudio(alarmPlayer, sysDefAlarmSound.value);
     } else { pauseAudio(alarmPlayer); }" src="<?=$prefix.'call.png';?>">
