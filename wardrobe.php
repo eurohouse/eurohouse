b505abe4ffaedba6b05d192e701ca10ed0d48386
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-12-20 19:55:48 +1000

    Latest

--- wardrobe.php
+++ wardrobe.php
@@ -229,7 +229,7 @@ tfoot {
     color: var(--backtextcolor);
     font-family: "euro"; font-weight: normal;
     font-size: var(--backsize); text-align: center;
-    position: sticky; top: 1; z-index: 1;
+    position: sticky; top: 0; z-index: 1;
 }
 img { position: relative; }
 input[type=button], input[type=image], button {
