7bf3fb2974f1d9dc754eb03f00ff2f617123df0c
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-12-28 22:23:11 +1000

    Latest

--- mode.user_tutorial.php
+++ mode.user_tutorial.php
@@ -2,9 +2,16 @@
 <!-- GR: Εγχειρίδιο Χρήσης; CY: Εγχειρίδιο Χρήσης; FR: Manuel d'utilisation; BE: Manuel d'utilisation; DE: Benutzerhandbuch; AT: Benutzerhandbuch; CH: Manualis Usuario; IT: Manuale d'uso; ES: Manual de usuario; MX: Manual de usuario; PT: Manual do utilizador; BR: Manual do usuário; RO: Manual de utilizare; MD: Manual de utilizare; RU: Руководство пользователя; NP: བཀོལ་སྤྱོད་ལག་དེབ།; RS: Упутство за употребу; UA: Посібник користувача; IN: उपयोगकर्ता गाइड; TR: Kullanıcı Kılavuzu; LK: उपयोक्तृपुस्तिका; CN: 用户指南; KR: 지도 시간; JP: チュートリアル; AE: دليل المستخدم. SA: دليل المستخدم. IQ: دليل المستخدم. IR: دليل المستخدم. KW: دليل المستخدم. QA: دليل المستخدم. -->
 <script>
 function openHelpPage(id) {
-    var tutd = jsonstr(sysDefTutorData.value);
-    var tutm = (tutd[id]['language'][sysDefUnits.value]['memoir'] !== undefined) ? tutd[id]['language'][sysDefUnits.value]['memoir'] : tutd[id]['memoir'];
-    helpContent.innerText = tutm;
+    var tutd = jsonstr(sysDefTutorData.value), tutm = '';
+    if ((tutd[id] !== undefined) && (tutd[id][sysDefUnits.value] !== undefined) && (tutd[id][sysDefUnits.value]['title'] !== undefined)) {
+        tutm = tutd[id][sysDefUnits.value]['title'];
+    } else {
+        if (tutd[id]['default']['title'] !== undefined) {
+            tutm = tutd[id]['default']['title'];
+        } else {
+            tutm = '';
+        }
+    } helpContent.innerText = tutm;
 }
 </script>
 <div class="notesRow">
