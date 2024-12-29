e89f60f853d63891cf713774facd1d7abd98a9f4
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-12-29 10:29:51 +1000

    Latest

--- startup.php
+++ startup.php
@@ -302,7 +302,6 @@ function automator() {
     var frndPower = arrjob(sysDefFriendData.value,';',':');
     var toolPower = arrjob(sysDefToolData.value,';',':');
     var userList = (sysDefUsersList.value).split(',');
-    console.log(sysDefUsersList.value);
     var subName = userList[rand(0, userList.length)];
     var objName = userList[rand(0, userList.length)];
     var subFrnd = friendsOf(frndPower, subName);
@@ -312,11 +311,11 @@ function automator() {
     } if ((subName != '') && (objName != '') && (isInt(tabPower[subName])) && (tabPower[subName] >= 0) && (autoPower[subName] == 'auto')) {
         sut = jsonMarket(subName, '!account,password');
         sch = Object.keys(sut)[rand(0, Object.keys(sut).length-1)];
-        console.log('SUBJECT: '+sch);
+        console.log('SUBJECT @'+subName+': '+sch);
         bind(subName, objName); equip(subName, sch);
         obt = jsonMarket(objName, '!account,password');
         och = Object.keys(obt)[rand(0, Object.keys(obt).length-1)];
-        console.log('OBJECT: '+och);
+        console.log('OBJECT @'+objName+': '+och);
         if ((subName != objName) && (storeOpen(objName))) {
             buy_item(subName, och, objName);
         } if ((subName != objName) && (sut[sch]['type'] !== undefined) && (sut[sch]['type'] == 'weapon') && (!(subFrnd.includes(objName)))) {
