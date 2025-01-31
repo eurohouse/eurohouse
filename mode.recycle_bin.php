0624f4123ed3cbd0af8f54c50bbc83e3fcfdce18
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-01-31 23:53:43 +1000

    Latest update

--- mode.recycle_bin.php
+++ mode.recycle_bin.php
@@ -1,15 +1,15 @@
 <!-- delete -->
 <!-- CH: Redivivus Bin; FR: Corbeille; BE: Corbeille; RO: Coșul de reciclare; MD: Coșul de reciclare; BR: Lixeira de reciclagem; PT: Contentor; LK: पुनर्चक्रण बिन; IN: रीसायकल बिन; TR: Geri Dönüşüm Kutusu; ES: Papelera de reciclaje; MX: Papelera de reciclaje; IT: Pattumiera; DE: Papierkorb; AT: Papierkorb; GR: Κάδος Ανακύκλωσης; CY: Κάδος Ανακύκλωσης; RU: Корзина; UA: Кошик; CN: 回收站; KR: 휴지통; JP: ごみ箱; AE: إمساك الدفاتر -->
 <?php
-$line1Size = 70; $recycleDir = './.trash/';
-$recycleList = str_replace($recycleDir,'',(glob($recycleDir.'*_fragment.json')));
-$elemString = term('Total elements:', $settings['vocabulary'], $session['units']).' '.count($recycleList);
-$sizeRecBin = 0; foreach ($recycleList as $val) {
-    $sizeRecBin += filesize($val);
-} $sizeString = sizestr($sizeRecBin, $settings['locale']['size'], $session['units']);
+$line1Size=70;$recycleDir='./.trash/';
+$recycleList=str_replace($recycleDir,'',(glob($recycleDir.'*_fragment.json')));
+$elemString=term('Total elements:',$settings['vocabulary'],$session['units']).' '.count($recycleList);
+$sizeRecBin=0; foreach ($recycleList as $val) {
+    $sizeRecBin+=filesize($val);
+} $sizeString=sizestr($sizeRecBin,$settings['locale']['size'],$session['units']); $diskSpace=sizestr(disk_free_space('/'),$settings['locale'],$session['units']);
 ?>
 <p align="center">
-    <?=$elemString;?><br><?=$sizeString;?> <input type="button" value="<?=term('Clear', $settings['vocabulary'], $session['units']);?>" onmouseover="soundButton();" onclick="getdir('d','','.trash','from','','here',false);">
+    <?=$elemString;?><br><?=$sizeString;?> <input type="button" value="<?=term('Clear', $settings['vocabulary'], $session['units']);?>" onmouseover="soundButton();" onclick="getdir('d','','.trash','from','','here',false);"><br><?=$diskSpace;?>
 </p>
 <?php foreach ($recycleList as $val) { ?>
     <p align="center" class="block">
