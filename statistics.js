15557c64dfa5a1b5f93a90eb7187de8eed3a2fb8
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-04 13:32:14 +1000

    Adds

--- statistics.js
+++ statistics.js
@@ -150,7 +150,7 @@ function scores(sta) {
             if ((sortable[indi]!==undefined)&&(indi!='')) {
                 res+="<input type='image' class='power' src='Flag."+indi+".png"+"'>";
                 res+="<input type='button' style='width:15%;' value='"+indi+"' onclick='navigator.clipboard.writeText(&#34;"+indi+"&#34;);'>";
-                res+="<input type='button' style='width:22%;' value='"+sortable[indi]['Human Development Index']+"' onclick='navigator.clipboard.writeText(&#34;"+sortable[indi]['Human Development Index']+"&#34;);'>";
+                res+="<input type='button' style='width:24%;' value='"+sortable[indi]['Human Development Index']+"' onclick='navigator.clipboard.writeText(&#34;"+sortable[indi]['Human Development Index']+"&#34;);'>";
                 res+="<input type='button' style='width:40%;' value='$"+sortable[indi]['$DISP']['%G']+"' onclick='navigator.clipboard.writeText(&#34;"+sortable[indi]['$DISP']['%G']+"&#34;);'>";
                 res+="<input type='image' class='power' src='"+epr+"info.png"+"' onclick='omniPath(&#34;i18n.json&#34;,&#34;"+indi+"&#34;,&#34;false&#34;);'><br>";
             }
