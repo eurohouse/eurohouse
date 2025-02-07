29376644f0fcfbdbcd0469bbd0a2ec2906b1d1bf
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-07 21:41:50 +1000

    Adds

--- statistics.js
+++ statistics.js
@@ -1,7 +1,7 @@
-function hdiscore(lem=(65-2.5),lef=(65+2.5),mys=(15/2),eys=(18/2),gni=100) {
+function hdiscore(lem,lef,mysm,mysf,eysm,eysf,gnim,gnif) {
     var lei=((((lem+lef)/2)-20)/65);
-    var ei=(((mys/15)+(eys/18))/2);
-    var ii=((Math.log(gni)-Math.log(100))/Math.log(750));
+    var ei=(((((mysm+mysf)/2)/15)+(((eysm+eysf)/2)/18))/2);
+    var ii=((Math.log((gnim+gnif)/2)-Math.log(100))/Math.log(750));
     return superRound((lei*ei*ii)**(1/3));
 }
 function scores(sta) {
