106c2c7179f500f5d612ada0c96b65ec212bfa3a
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-07 22:04:45 +1000

    Adds

--- statistics.js
+++ statistics.js
@@ -140,7 +140,7 @@ function scores(sta) {
             } var lei=((((lem+lef)/2)-20)/65);
             var ei=(((((mysm+mysf)/2)/15)+(((eysm+eysf)/2)/18))/2);
             var ii=((Math.log((gnim+gnif)/2)-Math.log(100))/Math.log(750));
-            console.log(ii);
+            console.log(gnim+' '+gnif);
             obj[et]['Human Development Index']=Math.round((lei*ei*ii)**(1/3));
             obj[et]['Gross National Income']['Both']=superRound((gnim+gnif)/2);
         } sortable=Object.fromEntries(
