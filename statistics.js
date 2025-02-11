1ffeaddd4775e33e055542402df62bca120d6d75
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-11 15:39:20 +1000

    Adds

--- statistics.js
+++ statistics.js
@@ -127,9 +127,9 @@ function scores(sta) {
         ); for (indi in sortable) {
             if ((sortable[indi]!==undefined)&&(indi!='')) {
                 res+="<input type='image' class='power' src='Flag."+sortable[indi]['%cc']+".png"+"'>";
-                res+="<input type='button' style='width:30%;' value='"+indi+"' onclick='clp(&#34;"+indi+"&#34;);'>";
+                res+="<input type='button' style='width:30%;' value='"+sortable[indi]['%zd']+" "+indi+"' onclick='clp(&#34;"+indi+"&#34;);'>";
                 res+="<input type='button' style='width:24%;' value='"+timeto(sortable[indi]['%bd'])+"' onclick='clp(&#34;"+sortable[indi]['%bd']+"&#34;);'>";
-                res+="<input type='button' style='width:20%;' value='"+sortable[indi]['%zd']+" "+sortable[indi]['%fr']+"' onclick='clp(&#34;"+sortable[indi]['%zd']+"&#34;);'>";
+                res+="<input type='button' style='width:20%;' value='"+sortable[indi]['%fr']+"' onclick='clp(&#34;"+sortable[indi]['%fr']+"&#34;);'>";
                 res+="<input type='image' class='power' src='"+epr+"chain.png"+"' onclick='omniReadGroup(&#34;browse_europedia&#34;,&#34;"+indi+"&#34;);'><br>";
             }
         }
