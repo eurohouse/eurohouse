b5afd62f2a1ac26c267ddd5232f0ffa7b4b899e9
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-13 22:51:17 +1000

    Added

--- mode.family_manager.php
+++ mode.family_manager.php
@@ -1,14 +1,14 @@
 <!-- access -->
 <!-- GR: Τοπικοί χρήστες και ομάδες; DE: Lokale Benutzer und Gruppen; AT: Lokale Benutzer und Gruppen; CY: Τοπικοί χρήστες και ομάδες; CH: Administrare Usuarem; ES: Usuarios locales y grupos; MX: Usuarios locales y grupos; FR: Utilisateurs et groupes locaux; BE: Utilisateurs et groupes locaux; TR: Yerel kullanıcılar ve gruplar; IT: Utenti e gruppi locali; RO: Utilizatori și grupuri locale; MD: Utilizatori și grupuri locale; LK: स्थानीयप्रयोक्तारः समूहाः च; IN: स्थानीय उपयोगकर्ता और समूह; RU: Локальные пользователи и группы; RS: Локални корисници и групе; NP: ས་གནས་ཀྱི་སྤྱོད་མཁན་དང་སྡེ་ཚན།; BR: Usuários e grupos locais; PT: Utilizadores e grupos locais; UA: Локальні користувачі та групи; CN: 本地用户和组; KR: 로컬 사용자; JP: ローカルユーザ; AE: المستخدمون المحليون -->
 <?php
-$maple=arropen($sessionID.'_maple.json',"{[{\"value\":\"\"}]}");
+$maple=arropen($sessionID.'_maple.json');
 ?>
 <table id="maple" style="width:100%;">
 <tbody>
-    <?php foreach ($maple as $row) { ?>
+    <?php foreach ($maple as $key=>$ent) { ?>
     <tr>
-        <?php foreach ($row as $col) { ?>
-            <td><?=$col['value'];?></td>
+        <?php foreach ($ent as $eno=>$val) { ?>
+            <td><?=$val;?></td>
         <?php } ?>
     </tr>
     <?php } ?>
