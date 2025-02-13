c900a07a2f1bb6f021e6b6dcf2a1a601bc372878
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-13 22:38:26 +1000

    Adds

--- mode.family_manager.php
+++ mode.family_manager.php
@@ -1,19 +1,7 @@
 <!-- access -->
 <!-- GR: Τοπικοί χρήστες και ομάδες; DE: Lokale Benutzer und Gruppen; AT: Lokale Benutzer und Gruppen; CY: Τοπικοί χρήστες και ομάδες; CH: Administrare Usuarem; ES: Usuarios locales y grupos; MX: Usuarios locales y grupos; FR: Utilisateurs et groupes locaux; BE: Utilisateurs et groupes locaux; TR: Yerel kullanıcılar ve gruplar; IT: Utenti e gruppi locali; RO: Utilizatori și grupuri locale; MD: Utilizatori și grupuri locale; LK: स्थानीयप्रयोक्तारः समूहाः च; IN: स्थानीय उपयोगकर्ता और समूह; RU: Локальные пользователи и группы; RS: Локални корисници и групе; NP: ས་གནས་ཀྱི་སྤྱོད་མཁན་དང་སྡེ་ཚན།; BR: Usuários e grupos locais; PT: Utilizadores e grupos locais; UA: Локальні користувачі та групи; CN: 本地用户和组; KR: 로컬 사용자; JP: ローカルユーザ; AE: المستخدمون المحليون -->
-<?php $std=[
-    [
-        [
-            "title"=>"Subject"
-        ],
-        [
-            "title"=>"Head"
-        ],
-        [
-            "title"=>"Body"
-        ]
-    ]
-];
-$maple=arropen($sessionID.'_maple.json',json_encode($std));
+<?php
+$maple=arropen($sessionID.'_maple.json',"{[{\"value\":\"\"}]}");
 ?>
 <table id="maple" style="width:100%;">
 <tbody>
