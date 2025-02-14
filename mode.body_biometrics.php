06b3668dfc64c87153346d5583a284a5c968a472
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-14 11:34:04 +1000

    Adds

--- mode.body_biometrics.php
+++ mode.body_biometrics.php
@@ -1,5 +1,6 @@
 <!-- access -->
 <!-- GR: Βιομετρία σώματος; DE: Körperbiometrie; AT: Körperbiometrie; CY: Βιομετρία σώματος; CH: Corpus Biometrics; ES: Biometría corporal; MX: Biometría corporal; FR: Biométrie corporelle; BE: Biométrie corporelle; TR: Vücut Biyometrikleri; IT: Biometria corporea; RO: Biometria corpului; MD: Biometria corpului; LK: शरीर बायोमेट्रिक्स; IN: शारीरिक बायोमेट्रिक्स; RU: Телесная биометрия; RS: Биометрија тела; NP: གཟུགས་པོའི་སྐྱེ་དངོས་ཚད་འཇལ།; BR: Biometria Corporal; PT: Biometria Corporal; UA: Біометрія тіла; CN: 身体生物识别; KR: 신체 생체 인식; JP: 身体生体認証; AE: القياسات الحيوية للجسم -->
+<?php if (isAuth()) { ?>
 <script>
 function mapleExec(input) {
     var mc=input.split(': ');
@@ -37,3 +38,4 @@ function mapleDel(id) {
     <?php } ?>
 </tbody>
 </table>
+<?php } ?>
