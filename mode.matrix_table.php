214d14cad984474fd32bff195dff95f4887586a9
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-02-27 12:27:41 +1000

    Added

--- mode.matrix_table.php
+++ mode.matrix_table.php
@@ -1,5 +1,3 @@
 <!-- access -->
 <!-- GR: Πίνακας Μήτρας; DE: Matrixtabelle; AT: Matrixtabelle; CY: Πίνακας Μήτρας; CH: Matrix Mensa; ES: Mesa Matricial; MX: Mesa Matricial; FR: Tableau Matriciel; BE: Tableau Matriciel; TR: Matris Tablosu; IT: Tabella Matrice; RO: Tabelul Matricei; MD: Tabelul Matricei; LK: मैट्रिक्स सारणी; IN: मैट्रिक्स तालिका; RU: Матричная таблица; RS: Табела матрица; NP: མེ་ཊིག་སི་རེའུ་མིག; BR: Tabela de Matriz; PT: Tabela de Matriz; UA: Матрична таблиця; CN: 用户特定矩阵表; KR: 매트릭스 테이블; JP: マトリックステーブル; AE: جدول المصفوفات -->
-<div class="customPanel" style="width:100%;height:60%;left:0px;top:0px;overflow-y:scroll;" id="pressAnyKey" onkeydown="pressedKeyCode.innerText=event.keyCode;">
 <h1 align="center" id="pressedKeyCode"></h1>
-</div>
