387f34b45ee5ac2418bc0ef6692c570d8026c245
Vadim Ponomarev <59008533+jesuscaesar@users.noreply.github.com>
2025-02-28 00:10:26 +1000

    Update wardrobe.php

--- wardrobe.php
+++ wardrobe.php
@@ -134,7 +134,7 @@ body {
 }
 .marquee {
     overflow: hidden; white-space: nowrap; cursor: pointer;
-    display: inline-block; animation: scrollMarquee 30s linear infinite;
+    display: inline-block; animation: scrollMarquee 60s linear infinite;
 }
 .panel {
     border: none; border-radius: 0px; position: relative;
