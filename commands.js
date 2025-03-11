70e548d3a7dc360f372e61ff79789ac8f405c16c
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-03-12 00:15:49 +1000

    Added

--- commands.js
+++ commands.js
@@ -183,12 +183,18 @@ function pipeExec(input) {
                 } inc=0; if (((it==1)&&(it==brd))||((it==1)&&(it<brd))) {
                     for (j=0; j<ml.length; j++) {
                         if ((ml[j].toLowerCase().includes(np3.toLowerCase()))&&(isInt(np4))) {
-                            if ((isInt(np2))&&(inc>=np2)) {
-                                omniListen(ml[j],true,parseInt(np4));
-                                break;
-                            } else if (np2=='*') {
-                                omniListen(mr[rand(0,mr.length)],true,parseInt(np4));
-                                break;
+                            if (input.endsWith('\\')) {
+                                if ((isInt(np2))&&(inc>=np2)) {
+                                    omniListen(ml[j],true,parseInt(np4));break;
+                                } else if (np2=='*') {
+                                    omniListen(mr[rand(0,mr.length)],true,parseInt(np4));break;
+                                }
+                            } else {
+                                if ((isInt(np2))&&(inc>=np2)) {
+                                    ard=arrangeMenu(sysDefUpNext.value,etw(ml[j],sysDefSessionID.value,sysDefNumeric.value),' | '); setdata('up_next',ard); break;
+                                } else if (np2=='*') {
+                                    ard=arrangeMenu(sysDefUpNext.value,etw(mr[rand(0,mr.length)],sysDefSessionID.value,sysDefNumeric.value),' | '); setdata('up_next',ard); break;
+                                }
                             } inc++;
                         } omniPause();
                     }
