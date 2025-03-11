b27da37eb0782ca5d2cd25aa5bd78da07b7d8e40
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-03-11 18:20:21 +1000

    Added

--- frontend.php
+++ frontend.php
@@ -142,17 +142,21 @@ function storeHours(id) {
     var userNum=arraySearch(id,userArr);
     return pager(sysDefHoursActive.value,userNum);
 }
+function isInBackup() {
+    return ((localStorage.getItem(ss+'_backup')!==undefined)&&(localStorage.getItem(ss+'_storage')===undefined));
+}
 function userBackup() {
     var ss=sysDefSessionID.value,rf={};
-    if (localStorage.getItem(ss+'_backup')!==undefined) {
+    if (isInBackup()) {
         copy(ss+'_backup.json',ss+'_session.json',true,1);
         readFile(ss+'_session.json','read','',ss+'_session');
         rf=jsonarr(localStorage.getItem(ss+'_session'));
-        omniListen(dtw(rf['melody'],ss,rf['numeric']),false,parseInt(rf['current']));
+        omniListen(dtw(rf['melody'],ss,rf['numeric']),false,parseInt(rf['current'])); localStorage.removeItem(ss+'_backup');
     } else {
         copy(ss+'_session.json',ss+'_backup.json',true,1);
         readFile(ss+'_backup.json','read','',ss+'_backup');
         rf=jsonarr(localStorage.getItem(ss+'_backup'));
+        localStorage.removeItem(ss+'_session');
     }
 }
 function remove_entry(id,obj,name,complex=false,helper=false,dy=';',dx=':') {
