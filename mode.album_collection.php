e393c3d68fe48d499825c2cdeaef4c5fa32e8d1b
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-03-16 14:35:59 +1000

    Added

--- mode.album_collection.php
+++ mode.album_collection.php
@@ -1,9 +1,9 @@
 <!-- disk -->
 <!-- GR: Συλλογή λευκώματος; DE: Albumsammlung; AT: Albumsammlung; CY: Συλλογή λευκώματος; ES: Colección de álbumes; MX: Colección de álbumes; FR: Collection d'albums; BE: Collection d'albums; TR: Albüm Koleksiyonu; IT: Raccolta di album; RO: Colecția de albume; MD: Colecția de albume; LK: एल्बम संग्रह; IN: एल्बम संग्रह; RU: Коллекция альбомов; RS: Колекција албума; NP: ཆེད་སྒྲིག་ཕྱོགས་བསྒྲིགས།; BR: Coleção de álbuns; PT: Coleção de álbuns; UA: Колекція альбомів; CN: 专辑收藏; KR: 앨범 컬렉션; JP: アルバムコレクション; AE: مجموعة الألبومات -->
 <div class='customPanel' id='album_modes' style="width:100%;height:40px;left:0px;top:0px;"></div>
-<div class='customPanel' id='playlist_disp' style="width:100%;height:30%;left:0px;top:0px;overflow-y:scroll;">
+<div class='customPanel' id='playlist_disp' style="width:100%;height:25%;left:0px;top:0px;overflow-y:scroll;">
     <p id="currentPlaylist"></p>
 </div>
-<div class='customPanel' id='album_disp' style="width:100%;height:50%;left:0px;top:0px;overflow-y:scroll;">
+<div class='customPanel' id='album_disp' style="width:100%;height:55%;left:0px;top:0px;overflow-y:scroll;">
     <p id="currentAlbumList"></p>
 </div>
