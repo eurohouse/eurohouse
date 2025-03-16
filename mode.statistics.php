7927a9062103ea5f9e5fe5acf1eed93d1eb01255
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-03-16 13:14:49 +1000

    Added

--- mode.statistics.php
+++ mode.statistics.php
@@ -1,6 +1,6 @@
 <!-- status -->
 <!-- GR: Στατιστική; CY: Στατιστική; FR: Statistiques; BE: Statistiques; ES: Estadísticas; MX: Estadísticas; PT: Estatisticas; BR: Estatisticas; IT: Statistiche; DE: Statistiken; AT: Statistiken; CH: Statistica; LK: सांख्यिकी; RO: Statistici; MD: Statistici; NP: གྲངས་གཞི།; IN: आंकड़े; TR: İstatistik; UA: Статистика; RU: Статистика; RS: Статистика; CN: 用户统计; KR: 사용자 통계; JP: ユーザー統計; AE: إحصائيات المستخدم -->
-<div class='customPanel' id='stat_disp' style="width:100%;height:12%;left:0px;top:0px;">
+<div class='customPanel' id='stat_disp' style="width:100%;height:40px;left:0px;top:0px;">
     <p align='center'>
         <input type='image' id='switchBtnAuto' onmouseover="soundButton();" class="power" onclick="setdata('stats','auto');" src="<?=$prefix.'steer.png';?>">
         <input type='image' id='switchBtnCall' onmouseover="soundButton();" class="power" onclick="setdata('stats','call');" src="<?=$prefix.'dial.png';?>">
