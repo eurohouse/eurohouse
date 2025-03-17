41b6b9a40e431fd1541eebc6fd1e9abc6b1eb512
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-03-17 15:01:07 +1000

    Added

--- startup.php
+++ startup.php
@@ -136,12 +136,12 @@ function world_clock() {
                 } else if (sysDefAlbum.value=='avatar') {
                     epr=sysDefAvaPrefix.value,alr=listlock(sysDefAlbum.value);
                     for (iu in alr) {
-                        arl+="<input type='image' class='power' src='"+epr+alr[iu]+".png' title='"+alr[iu]+"' onclick='setdata(&#34;avatar&#34;,&#34;"+alr[iu]+"&#34;);'>";
+                        arl+="<input type='image' class='power' style='width:40px;height:40px;' src='"+epr+alr[iu]+".png' title='"+alr[iu]+"' onclick='setdata(&#34;avatar&#34;,&#34;"+alr[iu]+"&#34;);'>";
                     }
                 } else if (sysDefAlbum.value=='pictogram') {
                     epr=sysDefPrefix.value,alr=listlock(sysDefAlbum.value);
                     for (iu in alr) {
-                        arl+="<input type='image' class='power' src='"+epr+alr[iu]+".png' title='"+(alr[iu].toUpperCase)+"'>";
+                        arl+="<input type='image' class='power' style='width:40px;height:40px;' src='"+epr+alr[iu]+".png' title='"+(alr[iu].toUpperCase)+"'>";
                     }
                 } currentAlbumList.innerHTML=arl;
                 currentAlbumList.setAttribute('style','columns:'+albCol+';-webkit-columns:'+albCol+';-moz-columns:'+albCol+';');
