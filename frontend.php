deba74a06d02bbf9c4ac26db18d5e11901c1308c
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-05-15 20:55:12 +1000

    Adds

--- frontend.php
+++ frontend.php
@@ -600,7 +600,7 @@ function triggerResponse(usr,id,msg) {
         if (argv!==null) {
             msgbox.push('@'+usr);
             for (idx in argv) {
-                req=argv[idx].replace('{','').replace('}','');
+                req=argv[idx].toString().replace('{','').replace('}','');
                 msgbox.push('{'+req+'}');
                 msgbox.push('['+calc(req)+']');
             } compose(id,msgbox.join(' '));
