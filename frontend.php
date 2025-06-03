537276fb8cff4292cfa2a94123441b10fee2d871
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-06-03 13:30:03 +1000

    Added

--- frontend.php
+++ frontend.php
@@ -254,7 +254,6 @@ function init_user(id,pass=null) {
     if (!notNull(storeData)) {
         set('./'+id+'_store.json',JSON.stringify({}),'rw');
     } if (isLine(pass)) { set(id+'_password',pass,'rw'); }
-    delete_user(''); delete_user('0'); delete_user('auto');
 }
 function administer(entry,mode='+') {
     if (superuser()) {
