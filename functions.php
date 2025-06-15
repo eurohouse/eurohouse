bd6851195ba607d43c8dea1483722526a90c6e6e
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-06-15 17:58:55 +1000

    Ads

--- functions.php
+++ functions.php
@@ -345,12 +345,14 @@ function modelcard($id,$cont,$exem,$ses,$sti) {
             $annoT=$annoInd.' '.$tdiff;
         } $zodT=zodiacSign(date('j',strtotime($bday)));
         $dateT=chooseCalendar(strtotime($bday),$ses,$sti);
-        return [
+        $res=[
             'title'=>((isset($ent['birthday']))?$zodT.' '.$title:$title),
             'line'=>($zodT.' '.$title.' '.$annoT.' ('.$dateT.') '.$len.' '.$mas.' '.$body.' '.$shoe),
             'anno'=>$annoT,'body'=>$body,'shoe'=>$shoe,
             'height'=>$len,'weight'=>$mas
         ];
+    } else {
+        $res=[];
     }
 }
 function userlocks($arr,$col,array $pic) {
