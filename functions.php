24359f897ce06b8d0d7b10e37a8d8574dfcf0b19
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-06-29 18:10:51 +1000

    Added

--- functions.php
+++ functions.php
@@ -95,8 +95,8 @@ function express(array $for) {
             } $filepass=str_replace('./','',(glob('./*.md')));
             foreach ($filepass as $file) {
                 if (file_exists($file)) {
-                    chmod($file,0777); rename($file,$repo.'.md');
-                    chmod($repo.'.md',0777);
+                    chmod($file,0777); copy($file,$repo.'.md');
+                    unlink($file); chmod($repo.'.md',0777);
                 }
             } $filerest=str_replace('./','',(glob('./*.txt.bak')));
             foreach ($filerest as $file) {
