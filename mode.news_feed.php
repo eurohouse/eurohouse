d3a2ec951f545def6a5563bb74ff1ce687e82a5a
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-06-29 19:29:52 +1000

    Added

--- mode.news_feed.php
+++
@@ -1,8 +0,0 @@
-<!-- news -->
-<!-- GR: Ροή ειδήσεων; CY: Ροή ειδήσεων; IT: Feed di notizie; DE: Neuigkeiten; AT: Neuigkeiten; FR: Fil d'actualité; BE: Fil d'actualité; CH: Nuntia Feed; ES: Noticias; MX: Noticias; IN: समाचार फ़ीड; LK: समाचार फीड; UA: Стрічка новин; PT: Feed de notícias; BR: Feed de notícias; RO: Flux de știri; MD: Flux de știri; TR: Haber Akışı; RU: Новостная лента; NP: གསར་འགྱུར།; CN: 新闻提要; KR: 뉴스 피드; JP: ニュースフィード; AE: موجز الأخبار -->
-<!-- <ref> -->
-<!-- true -->
-<p align='center'>
-<iframe id="inlineframe" allow="fullscreen" loading="lazy" title="Inline Frame Example" style="width:100%;height:100%;position:relative;" src="<?=$request['input'];?>" onclick="inlineframe.requestFullscreen();">
-</p>
-</iframe>
