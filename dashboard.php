f039f7ce260abefb2ad2b1da51f4e5df12e44ab8
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-07-02 19:05:07 +1000

    Added

--- dashboard.php
+++ dashboard.php
@@ -33,7 +33,7 @@
         <input type="image" onmouseover="soundButton();" id="buttonBackspace" class="power" onclick="omniBox.value=''; omniBox.focus();" src="<?=$prefix[3].'backspace.png';?>" title="<?=term('Clear Console',$settings,$session);?>">
     <?php } else { ?>
         <input type="image" onmouseover="soundButton();" id="buttonSuggest" class="power" onclick="omniSuggest();" src="<?=$prefix[3].'dice.png';?>" title="<?=term('Suggest Username',$settings,$session);?>">
-        <input type='text' id="omniBoxAuthLogin" style="width:31%;" placeholder="<?=term('Username',$settings,$session);?>" value="" onkeydown="if (event.keyCode==13) {
+        <input type='text' id="omniBoxAuthLogin" style="width:30%;" placeholder="<?=term('Username',$settings,$session);?>" value="" onkeydown="if (event.keyCode==13) {
             omniBoxAuthPass.value=''; omniBoxAuthPass.focus();
         } else if (event.keyCode==27) { omniBoxAuthLogin.value='';
         } else if (event.keyCode==8) { handleInput(this.value);
@@ -41,7 +41,7 @@
         } else if (event.keyCode==113) { seekMusic(omniBoxAuthLogin.value);
         } else if (event.keyCode==115) { seekBanner(omniBoxAuthLogin.value);
         } keyPressed();" oninput="handleInput(this.value,true);">
-        <input type='password' id="omniBoxAuthPass" style="width:31%;" placeholder="<?=term('Password',$settings,$session);?>" value="" onkeydown="if (event.keyCode==13) {
+        <input type='password' id="omniBoxAuthPass" style="width:30%;" placeholder="<?=term('Password',$settings,$session);?>" value="" onkeydown="if (event.keyCode==13) {
             omniAuthRequest('signin',omniBoxAuthLogin.value,CryptoJS.SHA256(omniBoxAuthPass.value).toString());
         } else if (event.keyCode==27) {
             omniBoxAuthPass.value=''; omniBoxAuthLogin.focus();
