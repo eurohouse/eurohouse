3d568b95985f337ec29cb6a2a8a15eec621862d7
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-07-03 17:11:48 +1000

    Added

--- statistics.js
+++ statistics.js
@@ -185,9 +185,9 @@ function highscore(mode) {
         }
     } else if (mode=='work') {
         objText=sysDefPublicUserData.value,obj=jsonarr(objText)['wh'];
-        sortable=Object.fromEntries(
+        sortable=(notNull(obj))?Object.fromEntries(
             Object.entries(obj).sort(([,a],[,b])=>b-a)
-        ); for (indi in sortable) {
+        ):{}; for (indi in sortable) {
             if ((sortable[indi]!==undefined)||(indi!='')) {
                 res+="<input type='image' class='power' onmouseover='soundButton();' src='"+am+timezoner(indi,'at')+".png"+"' onclick='clip(&#34;"+timezoner(indi,'at')+"&#34;);'>";
                 res+="<input type='button' onmouseover='soundButton();' style='width:24%;' value='"+indi+"' onclick='clip(&#34;"+indi+"&#34;);'>";
