c6cb3ea927068f9c87b1f2ddf5ce2a1e0127770e
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-07-09 00:13:51 -0400

    Added new files

--- databox.php
+++ databox.php
@@ -6,7 +6,8 @@ $bindingData=fileopen('binding.json');
 $poweredData=fileopen('dominion.json');
 $autoData=fileopen('automator.json');
 $toolData=fileopen('toolbox.json');
-$pref=prefixes($userData); $locksArr=fileopen($cookie.'_files/lock.json',json_encode($userSettings['locks']));
+$pref=prefixes($userData);
+$locksArr=fileopen($cookie.'_files/lock.json',json_encode($userSettings['locks']));
 $userLocks=userlocks($locksArr,$userSettings['collections'],$pref); $notesArr=fileopen($cookie.'_files/metadata.json',json_encode($userSettings['metadata']));
 $listExem=exemplar(str_replace('./','',(glob('./*.models.json'))));
 foreach ($listExem as $key=>$value) {
@@ -28,26 +29,28 @@ $publicUserData=[]; foreach ($poweredData as $key=>$value) {
     if (file_exists($key.'_files/session.json')) {
         $testArr=fileopen($key.'_files/session.json',json_encode($userSettings['defaults']));
     } date_default_timezone_set(dec_tz($testArr['timezone']));
-    $publicUserData['hh'][$key]=date('H'); $publicUserData['hour'][$key]=date('H');
-    $publicUserData['date'][$key]=chooseCalendar(time(),$testArr,$userSettings); $publicUserData['time'][$key]=timedate(time(),$testArr,$userSettings);
-    foreach ($userSettings['public_user_data'] as $pub) {
+    $publicUserData['hh'][$key]=date('H');
+    $publicUserData['hour'][$key]=date('H');
+    /*$publicUserData['date'][$key]=chooseCalendar(time(),$testArr,$userSettings);
+    $publicUserData['time'][$key]=timedate(time(),$testArr,$userSettings);
+    */foreach ($userSettings['public_user_data'] as $pub) {
         $publicUserData[$pub][$key]=$testArr[$pub];
     }
 } $usersList=array_keys($poweredData);natcasesort($usersList);
-/* ¶ 0 */ echo $cookie."\r\n\r\n".
-/* ¶ 1 */ implode(',',$pref)."\r\n\r\n".
-/* ¶ 2 */ json_encode($bindingData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 3 */ json_encode($poweredData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 4 */ json_encode($autoData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 5 */ json_encode($toolData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 6 */ json_encode($userLocks,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 7 */ implode('//',(str_replace('./','',(glob('./*.mac')))))."\r\n\r\n".
-/* ¶ 8 */ implode('//',(str_replace('./','',(glob('./*.pro')))))."\r\n\r\n".
-/* ¶ 9 */ implode(',',$usersList)."\r\n\r\n".
-/* ¶ 10 */ json_encode($publicUserData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 11 */ json_encode($notesArr,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 12 */ json_encode($localesArr,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 13 */ json_encode($listExem,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 14 */ json_encode($listCont,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 15 */ json_encode($listDefExem,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
-/* ¶ 16 */ json_encode($listDefCont,JSON_UNESCAPED_UNICODE);
+echo $cookie."\r\n\r\n".
+implode(',',$pref)."\r\n\r\n".
+json_encode($bindingData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($poweredData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($autoData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($toolData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($userLocks,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+implode('//',(str_replace('./','',(glob('./*.mac')))))."\r\n\r\n".
+implode('//',(str_replace('./','',(glob('./*.pro')))))."\r\n\r\n".
+implode(',',$usersList)."\r\n\r\n".
+json_encode($publicUserData,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($notesArr,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($localesArr,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($listExem,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($listCont,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($listDefExem,JSON_UNESCAPED_UNICODE)."\r\n\r\n".
+json_encode($listDefCont,JSON_UNESCAPED_UNICODE);
