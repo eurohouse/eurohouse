35723bc2ebc271d6ae2e8a31d26b4463f4dd052f
Vadim Ponomarev <jesuscaesar@outlook.com>
2026-04-27 09:10:15 +1000

    Updated content

--- frontend.php
+++ frontend.php
@@ -319,7 +319,7 @@ function populateIpStats(req='') {
             if (countryIso) {
                 const img=document.createElement('img');
                 img.src=`Flag.${countryIso.toUpperCase()}.png`;
-                img.style.height='32px';
+                img.style.height='100%'; img.style.width='auto';
                 img.alt=`Flag of ${countryIso}`;
                 img.onerror=()=>{
                     img.style.display='none';
