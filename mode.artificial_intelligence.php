4fa117da188b29264219aeb5607503e93e8b1dfb
Vadim Ponomarev <jesuscaesar@outlook.com>
2026-08-18 19:36:10 +1000

    New version

--- mode.artificial_intelligence.php
+++ mode.artificial_intelligence.php
@@ -5,7 +5,7 @@
 } ?>
 <div class='customPanel' style="width:100%;height:1.7em;left:0px;top:0px;">
     <p align='center' class='block'>
-    <input type="text" id="promptGPT" style="width:50%;" placeholder="<?=term("Ask artificial intelligence anything",$settings,$session);?>" value="" onkeydown="if (event.keyCode==13) {
+    <input type="text" id="promptGPT" style="width:54%;" placeholder="<?=term("Ask artificial intelligence anything",$settings,$session);?>" value="" onkeydown="if (event.keyCode==13) {
         AI(this.value).then(reply=>{
             if (notNull(reply)) {
                 markdown_disp.innerHTML=marked.parse(reply);
@@ -17,8 +17,8 @@
     } else if (event.keyCode==8) { handleInput(this.value);
     } else if (event.keyCode==46) { handleInput(this.value);
     }" oninput="handleInput(this.value,true);">
-    <input type="image" class="power" id="buttonPrev" onmouseover="soundButton();" src="<?=$prefix[3].'prev.png';?>" onclick="soundClick(); var cur=parseInt(sysDefUserAICurNum.value); var max=parseInt(sysDefUserAIMaxNum.value); cur=(cur<=0)?max:(cur-1); sysDefUserAICurNum.value=cur; markdown_disp.innerHTML=marked.parse(getAIRecordContent(sysDefUserAICurNum.value)); promptGPT.focus();">
-    <input type="image" class="power" id="buttonNext" onmouseover="soundButton();" src="<?=$prefix[3].'next.png';?>" onclick="soundClick(); var cur=parseInt(sysDefUserAICurNum.value); var max=parseInt(sysDefUserAIMaxNum.value); cur=(cur>=max)?0:(cur+1); sysDefUserAICurNum.value=cur; markdown_disp.innerHTML=marked.parse(getAIRecordContent(sysDefUserAICurNum.value)); promptGPT.focus();">
+    <input type="image" class="power" id="buttonPrev" onmouseover="soundButton();" src="<?=$prefix[3].'prev.png';?>" onclick="soundClick(); var cur=parseInt(sysDefUserAICurNum.value); var max=parseInt(sysDefUserAIMaxNum.value); cur=(cur<=0)?max:(cur-1); sysDefUserAICurNum.value=cur; markdown_disp.innerHTML=marked.parse(getAIRecordContent(sysDefUserAICurNum.value));">
+    <input type="image" class="power" id="buttonNext" onmouseover="soundButton();" src="<?=$prefix[3].'next.png';?>" onclick="soundClick(); var cur=parseInt(sysDefUserAICurNum.value); var max=parseInt(sysDefUserAIMaxNum.value); cur=(cur>=max)?0:(cur+1); sysDefUserAICurNum.value=cur; markdown_disp.innerHTML=marked.parse(getAIRecordContent(sysDefUserAICurNum.value));">
     <input type="image" class="power" id="buttonKeyboard" onmouseover="soundButton();" src="<?=$prefix[3].'keyboard.png';?>" onclick="soundClick(); AI(this.value).then(reply=>{
     if (notNull(reply)) {
             markdown_disp.innerHTML=marked.parse(reply);
