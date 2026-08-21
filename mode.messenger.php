1a5b6635d7380a8832f27b9c9f336e59285948be
Vadim Ponomarev <59008533+holylance98@users.noreply.github.com>
2026-08-21 19:02:18 +1000

    Update mode.messenger.php

--- mode.messenger.php
+++ mode.messenger.php
@@ -6,12 +6,12 @@
     <input type="image" class="power" id="buttonMorseLock" onmouseover="soundButton();" src="<?=$prefix[3].(($session['morse']!=0)?'key.png':'lock.png');?>" onclick="soundClick(); setdata('morse',flip(sysDefMorse.value));">
     <input type="text" id="composeMessage" style="width:54%;" placeholder="<?=term("What's on your mind?",$settings,$session);?>" value="" onkeydown="if (event.keyCode==13) {
         compose(sysDefSessionID.value,composeMessage.value);
-    } else if (event.keyCode==27) { composeMessage.value=''; composeMessage.focus();
+    } else if (event.keyCode==27) { composeMessage.value='';
     } else if (event.keyCode==8) { handleInput(this.value);
     } else if (event.keyCode==46) { handleInput(this.value);
     }" oninput="handleInput(this.value,true);">
     <input type="image" class="power" id="buttonKeyboard" onmouseover="soundButton();" src="<?=$prefix[3].'keyboard.png';?>" onclick="soundClick(); compose(sysDefSessionID.value,composeMessage.value);">
-    <input type="image" class="power" id="buttonBackspace" onmouseover="soundButton();" src="<?=$prefix[3].'backspace.png';?>" onclick="soundClick(); composeMessage.value=''; composeMessage.focus();">
+    <input type="image" class="power" id="buttonBackspace" onmouseover="soundButton();" src="<?=$prefix[3].'backspace.png';?>" onclick="soundClick(); composeMessage.value='';">
 </p>
 </div>
 <div class='customPanel' id='msgBox' style="width:100%;height:85%;left:0px;top:0px;overflow-y:scroll;">
