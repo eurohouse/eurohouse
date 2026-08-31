61d7019b48805c4af27a7e09bd20c1532a33d8eb
Vadim Ponomarev <jesuscaesar@outlook.com>
2026-08-31 17:32:59 +1000

    Minor fixes

--- mode.volume_control.php
+++ mode.volume_control.php
@@ -1,7 +1,7 @@
 <!-- volume -->
 <!-- AD: Niveles de volumen; AG: Niveles de volumen; AT: Lautstärkeregelung; BE: Niveaux de volume; BR: Niveles de volumen; CH: Occasus Acusticus; CL: Niveles de volumen; CN: 声音混合器; CO: Niveles de volumen; CU: Niveles de volumen; CY: Ακουστική; DD: Lautstärkeregelung; DE: Lautstärkeregelung; DR: Lautstärkeregelung; ES: Niveles de volumen; FR: Niveaux de volume; GR: Ακουστική; IN: वॉल्यूम स्तर; IT: Livelli di volume; JP: サウンドミキサー; KP: 사운드 믹서; KR: 사운드 믹서; LK: आयतनस्तराः; MC: Niveaux de volume; MD: Niveluri de volum; MX: Niveles de volumen; PT: Niveles de volumen; RO: Niveluri de volum; RS: Нивои јачине звука; RU: Уровни громкости; SM: Livelli di volume; SP: Occasus Acusticus; TR: Ses seviyeleri; UA: Рівні гучності; VA: Occasus Acusticus -->
-<div class="customPanel" style="width:100%;display:flex;flex-direction:column;flex-wrap:nowrap;">
-    <div class='customPanel' style="display:block;">
+<div class="customPanel" style="width:100%;display:grid;grid-template-columns:repeat(2,1fr);grid-template-rows:auto auto;">
+    <div class='customPanel'>
     <p align='center'>
         <label class="custom-checkbox-wrapper">
         <input type="checkbox" id="preservesPitchTumblr" class="custom-checkbox-input" value="<?=$session['preserves_pitch'];?>" <?=($session['preserves_pitch']!=0)?'checked':'';?> onchange="setdata('preserves_pitch',flip(sysDefPreservesPitch.value));">
@@ -10,7 +10,7 @@
         </label>
     </p>
     </div>
-    <div class='customPanel' style="display:block;">
+    <div class='customPanel'>
     <p align='center'>
         <label class="custom-checkbox-wrapper">
         <input type="checkbox" id="muteTumblr" class="custom-checkbox-input" value="<?=$session['mute'];?>" <?=($session['mute']!=0)?'checked':'';?> onchange="setdata('mute',flip(sysDefMute.value));">
