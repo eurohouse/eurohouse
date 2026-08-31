e2c60a418f54d0f9ef92daf22042233d268f87f9
Vadim Ponomarev <jesuscaesar@outlook.com>
2026-08-31 17:25:19 +1000

    Minor fixes

--- mode.volume_control.php
+++ mode.volume_control.php
@@ -1,6 +1,6 @@
 <!-- volume -->
 <!-- AD: Niveles de volumen; AG: Niveles de volumen; AT: Lautstärkeregelung; BE: Niveaux de volume; BR: Niveles de volumen; CH: Occasus Acusticus; CL: Niveles de volumen; CN: 声音混合器; CO: Niveles de volumen; CU: Niveles de volumen; CY: Ακουστική; DD: Lautstärkeregelung; DE: Lautstärkeregelung; DR: Lautstärkeregelung; ES: Niveles de volumen; FR: Niveaux de volume; GR: Ακουστική; IN: वॉल्यूम स्तर; IT: Livelli di volume; JP: サウンドミキサー; KP: 사운드 믹서; KR: 사운드 믹서; LK: आयतनस्तराः; MC: Niveaux de volume; MD: Niveluri de volum; MX: Niveles de volumen; PT: Niveles de volumen; RO: Niveluri de volum; RS: Нивои јачине звука; RU: Уровни громкости; SM: Livelli di volume; SP: Occasus Acusticus; TR: Ses seviyeleri; UA: Рівні гучності; VA: Occasus Acusticus -->
-<div class="customPanel" style="width:100%;display:grid;grid-template-columns:repeat(2,1fr);grid-template-rows:repeat(2,1fr);">
+<div class="customPanel" style="width:100%;display:flex;flex-direction:column;flex-wrap:nowrap;">
     <div class='customPanel'>
     <p align='center'>
         <label class="custom-checkbox-wrapper">
@@ -19,6 +19,8 @@
         </label>
     </p>
     </div>
+</div>
+<div class="customPanel" style="width:100%;display:grid;grid-template-columns:repeat(2,1fr);grid-template-rows:repeat(2,1fr);">
     <div class='customPanel'>
     <p align='center'>
         <input type='button' id="audioVolInd" value="<?=round(100*$session['audio_volume']).'%';?>"><br><?=term('Audio Volume',$settings,$session);?><br>
