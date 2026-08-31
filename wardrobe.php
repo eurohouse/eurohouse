4e0d2291bdb654f61e625da00dc50a4a7367c343
Vadim Ponomarev <jesuscaesar@outlook.com>
2026-08-31 17:38:26 +1000

    Minor fixes

--- wardrobe.php
+++ wardrobe.php
@@ -125,7 +125,7 @@ body {
     display: flex; flex-direction: column;
     align-items: center; position: relative;
     justify-content: center; text-align: center;
-    width: auto; height: 100%;
+    width: 100%; height: auto;
 }
 .grid-icon {
     position: relative; width: auto; height: 100%;
